60f7b9d02a9d3ea5063b896648f84039d0ea4978
syrine ben hassen <syrinebh1996@gmail.com>
2022-08-07 23:15:58 +0200

    pet

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CDBFooter, CDBBtn, CDBIcon, CDBBox } from 'cdbreact';
 import {Heart} from 'react-feather';
+import { Link, NavLink } from 'react-router-dom';
 
 export const Footer = () => {
   return (
@@ -26,17 +27,26 @@ export const Footer = () => {
           <small className="ml-2">&copy; Copy right to Syrine Ben Hassen <Heart/> . All rights reserved.</small>
         </CDBBox>
         <CDBBox display="flex">
+<a href='https://www.facebook.com/syrine.benhassen.507'>
           <CDBBtn flat color="dark" className="p-2">
+            
             <CDBIcon fab icon="facebook-f" />
         
           </CDBBtn>
+          </a>
+          <a href='https://github.com/sirinebhsn'>
           <CDBBtn flat color="dark" className="mx-3 p-2">
-            <CDBIcon fab icon="twitter" />
+            <CDBIcon fab icon="github" />
           </CDBBtn>
+          </a>
+          <a href='https://www.linkedin.com/in/syrine-ben-hassen-4b9b3a161/'>
           <CDBBtn flat color="dark" className="p-2">
-            <CDBIcon fab icon="instagram" />
+            <CDBIcon fab icon="linkedin" />
+
           </CDBBtn>
+          </a>
         </CDBBox>
+        
       </CDBBox>
     </CDBFooter>
   );
